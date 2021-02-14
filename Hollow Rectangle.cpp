23393a808726702f0e8e6751a30b90403254ed1a
AnirudhJS07 <76525607+AnirudhJS07@users.noreply.github.com>
2021-02-14 12:39:41 +0530

    Add files via upload
    This program will print a pattern of asterisks in a hollow rectangular manner.
    ****
    *   *
    *   *
    ****
    (Rows and columns based on input)

---
+++ Hollow Rectangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows,columns;
+    cout<<"Enter the number of rows for your rectangle: \n";
+    cin>> rows;
+    cout<<"Enter the number of columns for your rectangle: \n";
+    cin>> columns;
+
+    for(int i=1; i<=rows; i++)
+    {
+        for(int j=1; j<=columns; j++)
+            if(i==1||i==rows||j==1||j==columns)
+                {
+                    cout<<"*";
+                }
+            else
+                {
+                    cout<<" ";
+                }
+
+            cout<<endl;
+    }
+    return 0;
+}
