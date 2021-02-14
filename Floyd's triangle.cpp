59261d8b673b0f7a8d6b9717f6af66db1e3eb3dd
AnirudhJS07 <76525607+AnirudhJS07@users.noreply.github.com>
2021-02-14 13:31:48 +0530

    Self Created
    This program will print a pattern of numbers in the Floyd's triangle manner.
    Floyd's triangle is a special pattern of triangle where the number of elements every row increases by one and the elements are natural numbers starting from one.
    1
    2 3
    4 5 6
    7 8 9 10
    and so on.
    (Input based, as in the number of rows till you want the triangle to be)

---
+++ Floyd's triangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows, number=1;
+    cout<<"Enter number of rows:"<<endl;
+    cin>> rows;
+    for(int i=0; i<=rows; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout<<number<<" ";
+            ++number;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
