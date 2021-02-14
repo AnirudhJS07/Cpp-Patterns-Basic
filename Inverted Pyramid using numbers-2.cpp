377ba76858aacab6bac49008d46a6dc4da2181df
AnirudhJS07 <76525607+AnirudhJS07@users.noreply.github.com>
2021-02-14 13:29:02 +0530

    Self Created
    This program will print a pattern of numbers in a upper left aligned triangle manner.
    1 1 1 1 .... n times
    2 2 2... n-1 times
    ...
    n 1 time
    (Input based)

---
+++ Inverted Pyramid using numbers-2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter the last number upto which you want the pyramid to be:"<<endl;
+    cin>>a;
+
+    for(int i=1; i<=a; ++i)
+    {
+        for(int j=1; j<=a-i+1; ++j)
+        {
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
