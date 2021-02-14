fe90f092c0a2e0deb85863649c2c76765961b1ca
AnirudhJS07 <76525607+AnirudhJS07@users.noreply.github.com>
2021-02-14 13:39:44 +0530

    Self Created
    Advanced Pattern
    This program will print a pattern of asterisks in a diamond-like manner
    (Input based)

---
+++ Diamonds using stars.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"PRINT A DIAMOND"<<endl;
+    cout<<"The size of the diamond will be twice your input value"<<endl;
+    cout<<"Enter the size of the diamond you want: ";
+    cin>>n;
+    int a = (2*n-1)/2;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=a; j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=2*i-1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        a--;
+    }
+    a=0;
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=1; j<=a; j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=2*i-1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        a++;
+    }
+    return 0;
+}
