58289c94dd904b2b0940348f3406634e95a22676
AnirudhJS07 <76525607+AnirudhJS07@users.noreply.github.com>
2021-02-14 13:33:26 +0530

    Self Created
    This program will print a pattern of binary numbers(0 and 1) in a lower left aligned triangle manner.
    1
    0 1
    1 0 1
    0 1 0 1
    1 0 1 0 1
    and so on
    (Input based)

---
+++ 0-1 Pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    cout<<"Enter the number of rows for your Binary Pyramid: ";
+    cin>>k;
+    for(i=1; i<=k; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            if((i+j)%2==0)
+            {
+                cout<<"1 ";
+            }
+            else
+            {
+                cout<<"0 ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
